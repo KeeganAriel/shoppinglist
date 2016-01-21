a43741c45a8088cd2f8921e9a9e0355ccd75a03f
KeeganAriel <keegan.teinowitz@gmail.com>
2016-01-20 22:23:39 -0800

    css:formatting + attempt jq line-though

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
 	$("#add").click(function() {
 		var maininput = $("#typehere").val();
-			console.log(maininput);
-			
+		console.log(maininput);
+		if (maininput !== '') {
+			$("ul").append('<li><input type="checkbox">' + maininput + '<input type="number"></li>');
+			$('#typehere').val('');
+		}
 	});
+
+	$('[type="checkbox"]').click(function(){
+		var element = $(this).maininput();
+			if ($(this).is(':checked'))
+				  { element.wrap('<del>');
+				  } else {
+				  element.unwrap('<del>');
+					}
+		});
 
 
 });
