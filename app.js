953cc78e58811894c478fd2810c7c23f0fd62a35
KeeganAriel <keegan.teinowitz@gmail.com>
2016-01-25 16:15:33 -0800

    change px to em for mobile

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ $( document ).ready(function() {
 		var maininput = $("#typehere").val();
 		console.log(maininput);
 		if (maininput !== '') {
-			// var template = Handlebars.compile($('#list-item-template').html());
-			// $("ul").append(template(maininput));
 			$("ul").append('<li><input type="checkbox">' + maininput + '<input type="number"></li>');
 			$('#typehere').val('');
 		}
 		event.preventDefault();
 	});
-
 
 	$('.list').on ('click', '[type=checkbox]', function(){
 		$(this).parent().toggleClass('purchased');
