136cf2b84401215fa2573690345bc3e0c7b1fc10
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-27 11:12:02 -0700

    add font awesome change logo

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 $( document ).ready(function() {
+// add items to shopping list
 	$("form").submit(function(event) {
 		var maininput = $("#typehere").val();
-		console.log(maininput);
 		if (maininput !== '') {
-			// var template = Handlebars.compile($('#list-item-template').html());
-			// $("ul").append(template(maininput));
-			$("ul").append('<li><input type="checkbox">' + maininput + '<input type="number"></li>');
-			$('#typehere').val('');
+		
+		var source = $("#list-item-template").html();
+		var template = Handlebars.compile(source);
+
+			$("ul").append(template(maininput));
+			$('#typehere').val(''); //clear maininput
 		}
+		var count = $('.list li').length;
+		$('.item-count').text(count);
+
 		event.preventDefault();
 	});
 
-
+// checks items off in the shopping list
 	$('.list').on ('click', '[type=checkbox]', function(){
 		$(this).parent().toggleClass('purchased');
+		var count = $("input[type=checkbox]:checked").length;
+		$('.bought-item').text(count);
 	});
+
+
+
+		
+
 });
