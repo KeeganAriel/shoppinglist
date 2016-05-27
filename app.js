d24bbb721ed446f84e80275914840f5fcb51592a
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-27 11:15:09 -0700

    fix?

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ $( document ).ready(function() {
 	$("form").submit(function(event) {
 		var maininput = $("#typehere").val();
 		if (maininput !== '') {
-<<<<<<< HEAD
+
 			$("ul").append('<li><input type="checkbox">' + maininput + '<input type="number"></li>');
 			$('#typehere').val('');
-=======
+
 		
 		var source = $("#list-item-template").html();
 		var template = Handlebars.compile(source);
 
 			$("ul").append(template(maininput));
 			$('#typehere').val(''); //clear maininput
->>>>>>> gh-pages
 		}
 		var count = $('.list li').length;
 		$('.item-count').text(count);
@@ -21,10 +20,7 @@ $( document ).ready(function() {
 		event.preventDefault();
 	});
 
-<<<<<<< HEAD
-=======
 // checks items off in the shopping list
->>>>>>> gh-pages
 	$('.list').on ('click', '[type=checkbox]', function(){
 		$(this).parent().toggleClass('purchased');
 		var count = $("input[type=checkbox]:checked").length;
